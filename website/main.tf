f7c30b9192c1d1c37bbe8593ce79403b0aad6ed3
Markus Schmid <m.schmid@ryte.com>
2018-11-19 17:16:02 +0100

    cleanup dead code

--- website/main.tf
+++ website/main.tf
@@ -12,12 +12,4 @@ resource "aws_s3_bucket" "this" {
     error_document = "${var.website_error_document}"
     routing_rules  = "${var.website_routing_rules}"
   }
-
-  /*cors_rule {
-    allowed_headers = "${var.cors_allowed_headers}"
-    allowed_methods = "${var.cors_allowed_methods}"
-    allowed_origins = ["${var.cors_allowed_origins}"]
-    expose_headers  = "${var.cors_expose_headers}"
-    max_age_seconds = "${var.cors_max_age_seconds}"
-  }*/
 }
